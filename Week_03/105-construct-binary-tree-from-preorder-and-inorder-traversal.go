f03cea8d8b90076216ecb2e954193f8d47108176
Aki-Liang <11916467@qq.com>
2020-09-07 20:04:42 +0800

    完成105. 从前序与中序遍历序列构造二叉树

--- Week_03/105-construct-binary-tree-from-preorder-and-inorder-traversal.go
+++ Week_03/105-construct-binary-tree-from-preorder-and-inorder-traversal.go
@@ -1,1 +1,28 @@
 package Week_03
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) != len(inorder) || len(preorder) == 0 {
+		return nil
+	}
+
+	root := &TreeNode{
+		Val: preorder[0],
+	}
+	//找到中序遍历的跟节点
+	i := 0
+	for ; i < len(inorder); i++ {
+		if inorder[i] == preorder[0] {
+			break
+		}
+	}
+	root.Right = buildTree(preorder[len(inorder[:i])+1:], inorder[i+1:])
+	root.Left = buildTree(preorder[1:len(inorder[:i])+1], inorder[:i])
+
+	return root
+}
