8b8e25ba705f85626b88b18612908f2bb7f97c36
Aki-Liang <11916467@qq.com>
2020-09-18 12:58:23 +0800

    更新不拼接的解法

--- Week_04/74-search-a-2d-matrix.go
+++ Week_04/74-search-a-2d-matrix.go
@@ -21,3 +21,33 @@ func searchList(nums []int, target int) bool {
 		return searchList(nums[index+1:], target)
 	}
 }
+
+func searchMatrixV2(matrix [][]int, target int) bool {
+	m := len(matrix)
+	if m == 0 {
+		return false
+	}
+	n := len(matrix[0])
+	if n == 0 {
+		return false
+	}
+	if target > matrix[m-1][n-1] || target < matrix[0][0] {
+		return false
+	}
+	left, right := 0, m*n-1
+	for left <= right {
+		mid := left + (right-left)>>1
+		r := mid / n
+		c := mid % n
+		if matrix[r][c] == target {
+			return true
+		} else if matrix[r][c] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+
+	}
+
+	return false
+}
