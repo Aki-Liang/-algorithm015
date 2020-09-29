9d35ae88b7644ec75a37e76abe2cf7abbfff8644
Aki-Liang <11916467@qq.com>
2020-09-29 18:20:39 +0800

    完成青蛙跳河

--- Week_06/403-frog-jump.go
+++ Week_06/403-frog-jump.go
@@ -1,1 +1,24 @@
 package Week_06
+
+// 动态规划
+// 使用map来进行存储，key为石头位置，value保存可能挑到该石头的步数
+
+func canCross(stones []int) bool {
+	stepMap := make(map[int]map[int]int)
+	for _, stone := range stones {
+		stepMap[stone] = make(map[int]int)
+	}
+	stepMap[0] = map[int]int{0: 0}
+	for _, stone := range stones {
+		for step := range stepMap[stone] {
+			for i := step - 1; i <= step+1; i++ {
+				steps, ok := stepMap[stone+i]
+				if ok {
+					steps[i] = i
+				}
+			}
+		}
+	}
+	steps := stepMap[stones[len(stones)-1]]
+	return len(steps) > 0
+}
