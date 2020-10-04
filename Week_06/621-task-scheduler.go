f5601e650c7d6027f717b8e02c6f81203a9de86c
Aki-Liang <11916467@qq.com>
2020-10-04 19:53:01 +0800

    完成621

--- Week_06/621-task-scheduler.go
+++ Week_06/621-task-scheduler.go
@@ -1,1 +1,20 @@
 package Week_06
+
+import "sort"
+
+func leastInterval(tasks []byte, n int) int {
+	m := make([]int, 26)
+	for _, c := range tasks {
+		m[c-'A']++
+	}
+	sort.Ints(m)
+	maxVal := m[25] - 1
+	idleSlots := maxVal * n
+	for i := 24; i >= 0 && m[i] > 0; i-- {
+		idleSlots -= min(m[i], maxVal)
+	}
+	if idleSlots > 0 {
+		return idleSlots + len(tasks)
+	}
+	return len(tasks)
+}
